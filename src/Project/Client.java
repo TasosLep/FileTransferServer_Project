d68a2d687c51c76a7075c843b8a3a59882ddabd4
tasos <tasoslepipas@gmail.com>
2018-04-09 21:39:15 +0300

    Initial

--- src/Project/Client.java
+++ src/Project/Client.java
@@ -1,4 +1,65 @@
 package Project;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
 public class Client {
+
+    private  ObjectInputStream in;
+    private ObjectOutputStream out;
+    private Socket request_connection;
+    private Scanner scanner;
+
+    public void initializeClient(){
+
+        System.out.println("File Transfer Server platform!\n");
+
+        try {
+
+            //connecting to Server
+            System.out.println("Give me the IP Address of the Server you want to connect: ");
+            scanner = new Scanner(System.in);
+            String ipAddress = scanner.next();
+            System.out.println("Give me the port of the Server you want to connect: ");
+            scanner = new Scanner(System.in);
+            String portNymber = scanner.next();
+            int port = Integer.parseInt(portNymber.trim());
+            request_connection = new Socket(ipAddress,port);
+            //end of connecting to Server
+
+            in = new ObjectInputStream(request_connection.getInputStream());
+       //     out = new ObjectOutputStream(request_connection.getOutputStream());
+
+            Object welcome = in.readObject();//Reading Welcoming
+            System.out.println(welcome);
+
+
+
+        }
+        catch (UnknownHostException unknownHost) {
+            System.err.println("You are trying to connect to an unknown host!");
+        } catch (Exception ioException) {
+            ioException.printStackTrace();
+        }
+        finally {
+            try {
+                in.close();
+             //   out.close();
+                request_connection.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+
+    public static void main(String args[]){
+        new Client().initializeClient();
+    }
 }
--- src/Project/Server.java
+++ src/Project/Server.java
@@ -19,27 +19,44 @@ public class Server {
 
         try {
             serverSocket = new ServerSocket(666,1);
-            System.out.println("Server started...");
+            System.out.println("Server started...\n" + "Waiting for a Client\n");
 
         }
         catch (IOException e){
             System.out.println("Something went wrong when starting the server!");
         }
 
-        while (true){
+  //      while (true){
             try {
                 Socket socket = serverSocket.accept();
-                in = new ObjectInputStream(socket.getInputStream());
+             //   in = new ObjectInputStream(socket.getInputStream());
                 out = new ObjectOutputStream(socket.getOutputStream());
                 System.out.println("We have a new client connection...\n");
+                out.writeObject("Welcome!");
+                out.flush();
 
 
             }
             catch (IOException io){
                 System.out.println("Something went wrong with a client!");
+             //   break;
             }
 
-        }
+            finally {
+                try {
+                    //    in.close();
+                    out.close();
+                    serverSocket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+
+    //    }
+
+
+
 
 
     }
